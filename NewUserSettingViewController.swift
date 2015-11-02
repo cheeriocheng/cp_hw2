653b70acd9550bda770b6a7173ef21c49cbe47f2
cheng <gnuchex@gmail.com>
2015-11-01 22:52:16 -0800

    todo: add scrollview

---
+++ NewUserSettingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewUserSettingViewController.swift
+//  cp_hw2_db
+//
+//  Created by Xu, Cheng on 11/1/15.
+//  Copyright © 2015 sansserif. All rights reserved.
+//
+
+import UIKit
+
+class NewUserSettingViewController: UIViewController {
+    
+    
+    @IBOutlet var NewUserSettingViewController: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
