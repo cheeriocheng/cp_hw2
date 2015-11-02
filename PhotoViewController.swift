11d97f768a52825f81514adf1ab567f959c91392
cheng <gnuchex@gmail.com>
2015-11-02 11:13:08 -0800

    duplicated UI for logged in user. added log out button

--- PhotoViewController.swift
+++ PhotoViewController.swift
@@ -1,26 +1,24 @@
 //
-//  NewUserSettingViewController.swift
+//  PhotoViewController.swift
 //  cp_hw2_db
 //
-//  Created by Xu, Cheng on 11/1/15.
+//  Created by Xu, Cheng on 11/2/15.
 //  Copyright © 2015 sansserif. All rights reserved.
 //
 
 import UIKit
 
-class NewUserSettingViewController: UIViewController {
-    
-    
-    @IBOutlet var NewUserSettingViewController: UIView!
-    
-    
-    @IBOutlet weak var settingScrollView: UIScrollView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class PhotoViewController: UIViewController {
 
+
+ 
+    
+    @IBOutlet var photoUIScrollView: UIScrollView!
+    
+       override func viewDidLoad() {
+        super.viewDidLoad()
+        photoUIScrollView.contentSize = CGSize(width: 320 , height: 800);
         // Do any additional setup after loading the view.
-        settingScrollView.contentSize = CGSize(width: 320 , height: 772);
     }
 
     override func didReceiveMemoryWarning() {
